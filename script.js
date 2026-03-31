4552148264b9b61d81314a4dbc43c18fbc55645a
Ishwari Saddalgi <ishwarir2004@gmail.com>
2026-03-31 21:39:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener(DOMContentLoaded, function () {
+  console.log("Page loaded");
+
+  document.documentElement.style.scrollBehavior = "smooth";
+
+  document.querySelectorAll(".project-images").forEach((project) => {
+    if (!video.muted) {
+      video.muted = true;
+    }
+  });
+});
